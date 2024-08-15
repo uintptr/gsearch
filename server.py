51e1386817c7e716ee8a239214199ecd0c007aef
fsck <inaforloop@gmail.com>
2024-08-14 21:58:50 -0400

    using aiohttp

--- server.py
+++ server.py
@@ -9,9 +9,12 @@ import asyncio
 import urllib.parse
 from http import HTTPStatus
 
+import aiohttp
 from jsonconfig import JSONConfig
 
-from ahttp.ahttp import AsyncHttpRequest, AsyncHttpServer, AsyncHttpClient
+
+from aiohttp import web
+
 
 DEF_CACHE_TIMEOUT = (1 * (60 * 60))
 DEF_PORT = 8080
@@ -34,106 +37,7 @@ def printkv(k: str, v: object) -> None:
     print(f"    {k:<25}{v}")
 
 
-class ClientRequestHandler:
-    def __init__(self, url: str, method: str = "GET") -> None:
-        self.connections_lock = asyncio.Lock()
-        self.connections: list[AsyncHttpClient] = []
-        self.url = url
-
-        if ("GET" == method):
-            self.is_get = True
-        else:
-            self.is_get = False
-
-    async def _pop_client_connection(self) -> AsyncHttpClient:
-
-        async with self.connections_lock:
-            if (len(self.connections) > 0):
-                return self.connections.pop()
-            else:
-                client = AsyncHttpClient(self.url)
-                await client.connect()
-                return client
-
-    async def _return_client_connection(self, client: AsyncHttpClient) -> None:
-
-        async with self.connections_lock:
-            self.connections.append(client)
-
-    async def __aenter__(self) -> 'ClientRequestHandler':
-        return self
-
-    async def __aexit__(self, type, value, traceback) -> None:  # type: ignore
-        await self.close()
-
-    ############################################################################
-    # PUBLIC
-    ############################################################################
-    async def issue_request(self, params: dict[str, str] = {}, headers: dict[str, str] = {},  data: bytes = b'', max_attempts: int = 5) -> bytes:
-
-        url = self.url
-
-        count = 0
-        for k in params.keys():
-            v = urllib.parse.quote(params[k])
-
-            if (0 == count):
-                url += f"?{k}={v}"
-            else:
-                url += f"&{k}={v}"
-
-            count += 1
-
-        resp_data = b''
-        attempts = 0
-        replied = False
-        client = None
-
-        while (False == replied and attempts < max_attempts):
-
-            try:
-                client = await self._pop_client_connection()
-
-                for k in headers.keys():
-                    client.add_header(k, headers[k])
-
-                if (True == self.is_get):
-                    resp = await client.get(url)
-                else:
-                    resp = await client.post(url, data=data)
-
-                if (resp.status >= 200 and resp.status < 300):
-                    resp_data = await resp.read_all()
-                    replied = True
-                else:
-                    print(f"{url} returned {resp.status}")
-
-            except ConnectionAbortedError:
-                pass
-            except ConnectionError:
-                pass
-            except Exception as e:
-                print(f"Exception: {e}")
-            finally:
-                if (client is not None):
-                    if (True == replied):
-                        await self._return_client_connection(client)
-                    else:
-                        try:
-                            await client.close()
-                        except Exception as e:
-                            print(f"Exception: {e}")
-
-            attempts += 1
-        return resp_data
-
-    async def close(self) -> None:
-        while self.connections:
-            c = self.connections.pop()
-            await c.close()
-
-
-class AI(ClientRequestHandler):
+class AI:
 
     def __init__(self, config: JSONConfig) -> None:
 
@@ -141,15 +45,11 @@ class AI(ClientRequestHandler):
         self.system = config.get("/openai/system")
         self.temperature = config.get_float("/openai/temperature", 0.7)
         self.model = config.get("/openai/model", "gpt-3.5-turbo")
+        self.url = config.get("/openai/url")
 
-        url = config.get("/openai/url")
+    async def post(self, message: str) -> bytes:
 
-        super().__init__(url, method="POST")
-
-    async def do_request(self, message: str) -> bytes:
-
-        headers = {"Content-Type": "application/json",
-                   "Authorization": f"Bearer {self.key}"}
+        headers = {"Authorization": f"Bearer {self.key}"}
 
         messages = [
             {"role": "system", "content": self.system},
@@ -160,13 +60,13 @@ class AI(ClientRequestHandler):
                 "messages": messages,
                 "temperature": self.temperature}
 
-        data = json.dumps(data).encode("utf-8")
-
-        return await super().issue_request(headers=headers, data=data)
+        async with aiohttp.ClientSession() as session:
+            async with session.post(self.url, headers=headers, json=data) as response:
+                return await response.read()
 
     async def chat(self, message: str) -> str | None:
 
-        data = await self.do_request(message)
+        data = await self.post(message)
 
         completion = json.loads(data.decode("utf-8"))
 
@@ -215,23 +115,25 @@ class RedditCache:
         return None
 
 
-class GoogleRequestHandler(ClientRequestHandler):
+class GoogleRequestHandler:
 
     def __init__(self, config: JSONConfig) -> None:
 
         self.key = config.get("/google/key")
         self.cx = config.get("/google/cx")
         self.gl = config.get("/google/geo", "ca")
-        super().__init__(config.get("/google/url"))
+        self.url = config.get("/google/url")
 
-    async def do_request(self, q: str) -> bytes:
+    async def get(self, q: str) -> bytes:
 
         params = {"key": self.key,
                   "cx": self.cx,
                   "gl": self.gl,
                   "q": q}
 
-        return await super().issue_request(params)
+        async with aiohttp.ClientSession() as s:
+            async with s.get(self.url, params=params) as r:
+                return await r.read()
 
 
 class GCSEHandler:
@@ -246,34 +148,6 @@ class GCSEHandler:
         self.google_request = GoogleRequestHandler(config)
         self.ai = AI(config)
         self.reddit_cache = RedditCache(config, self.ai)
-
-    async def __aenter__(self) -> 'GCSEHandler':
-        return self
-
-    async def __aexit__(self, _, value, traceback) -> None:  # type: ignore
-        await self.google_request.close()
-
-    def _gzipped(self, data: bytes) -> bool:
-        return (data != b'' and data[0] == 0x1f and data[1] == 0x8b)
-
-    async def _favicon_get(self, url: str) -> bytes | None:
-
-        q = urllib.parse.urlparse(url)
-
-        try:
-            async with AsyncHttpClient(url) as client:
-
-                resp = await client.get(q.path)
-
-                if (resp.status >= 200 and resp.status < 300):
-                    return await resp.read_all()
-
-        except OSError:
-            print(f"Unable to connect to {url}")
-        except Exception as e:
-            print(f"{url} failed: {e}")
-
-        return None
 
     def __get_lucky_url(self, gcse_data: bytes) -> str | None:
 
@@ -292,20 +166,24 @@ class GCSEHandler:
     ############################################################################
     # PUBLIC
     ############################################################################
-    async def static_handler(self, req: AsyncHttpRequest) -> None:
 
-        fn = os.path.abspath(req.path)[1:]
+    async def static(self, request: web.Request) -> web.StreamResponse:
+
+        fn = os.path.abspath(request.path)[1:]
 
         if (fn == ""):
             fn = "index.html"
 
-        file_path = os.path.join(self.www_root, fn)
+        return web.FileResponse(os.path.join(self.www_root, fn))
 
-        await req.send_file(file_path)
-
-    async def search(self, req: AsyncHttpRequest, q: str) -> None:
+    async def search(self, req: web.Request) -> web.Response | web.FileResponse:
 
         location = None
+
+        if "q" not in req.rel_url.query:
+            return web.Response(status=HTTPStatus.BAD_REQUEST)
+
+        q = req.rel_url.query["q"]
 
         if q.startswith("r "):
             # reddit
@@ -322,82 +200,79 @@ class GCSEHandler:
             location = f"https://www.google.com/search?q={q}&tbm=isch"
         elif q.startswith("l "):
             # lucky
-            gcse_data = await self.google_request.do_request(q[2:])
+            gcse_data = await self.google_request.get(q[2:])
             location = self.__get_lucky_url(gcse_data)
 
         if location is not None:
-            req.add_header("Location", location)
-            req.set_status(HTTPStatus.MOVED_PERMANENTLY)
-            await req.send_headers()
-        else:
-            await req.send_file(os.path.join(self.www_root, "index.html"))
 
-    async def api_search(self, req: AsyncHttpRequest, q: str) -> None:
+            headers = {
+                "Location": location
+            }
 
-        if q == "test":
-            await req.send_file("tests/test.json")
-            return
+            return web.Response(headers=headers,
+                                status=HTTPStatus.MOVED_PERMANENTLY)
 
-        data = await self.google_request.do_request(q)
+        index = os.path.join(self.www_root, "index.html")
+        return web.FileResponse(index)
+
+    async def api_search(self, req: web.Request) -> web.Response:
+
+        if "q" not in req.rel_url.query:
+            return web.Response(status=HTTPStatus.BAD_REQUEST)
+
+        q = req.rel_url.query["q"]
+
+        data = await self.google_request.get(q)
 
         if (data != b''):
-            req.add_header("Content-Type", "application/json")
-            await req.send_data(data)
-        else:
-            req.set_status(HTTPStatus.NOT_FOUND)
+            return web.Response(body=data, content_type="application/json")
 
-    async def api_chat(self, req: AsyncHttpRequest, q: str) -> None:
+        return web.Response(status=HTTPStatus.NOT_FOUND)
 
-        data = await self.ai.do_request(q)
+    async def api_chat(self, req: web.Request) -> web.Response:
+
+        if "q" not in req.rel_url.query:
+            return web.Response(status=HTTPStatus.BAD_REQUEST)
+
+        q = req.rel_url.query["q"]
+
+        data = await self.ai.post(q)
 
         completion = json.loads(data.decode("utf-8"))
 
         if ("choices" in completion):
             if ("message" in completion["choices"][0]):
                 message = completion["choices"][0]["message"]
-                await req.send_as_json(message)
-            else:
-                req.set_status(HTTPStatus.REQUESTED_RANGE_NOT_SATISFIABLE)
-        else:
-            req.set_status(HTTPStatus.REQUESTED_RANGE_NOT_SATISFIABLE)
+                return web.Response(text=json.dumps(message),
+                                    content_type="application/json")
 
-        return
+            return web.Response(status=HTTPStatus.REQUESTED_RANGE_NOT_SATISFIABLE)
 
-    async def opensearch(self, req: AsyncHttpRequest) -> None:
+        return web.Response(status=HTTPStatus.REQUESTED_RANGE_NOT_SATISFIABLE)
+
+    async def opensearch(self, req: web.Request) -> web.Response:
 
         opensearch = OPEN_SEARCH_TEMPLATE.replace("__HOST__", req.host)
-
-        req.set_mime_type("application/xml")
-        await req.send_as_text(opensearch)
+        return web.Response(text=opensearch, content_type="application/xml")
 
 
-async def run_server(addr: str, port: int, verbose: bool, disable_cache: bool) -> None:
+def run_server(addr: str, port: int) -> None:
 
-    script_root = os.path.dirname(os.path.abspath(sys.argv[0]))
+    handler = GCSEHandler()
 
-    log_file = os.path.join(script_root, "gcse.log")
+    routes = [
+        web.get("/search", handler.search),
+        web.get("/opensearch.xml", handler.opensearch),
+        web.get("/api/search", handler.api_search),
+        web.get("/api/chat", handler.api_chat),
+        web.get("/{path:.*}", handler.static)
+    ]
 
-    async with GCSEHandler() as handler:
+    app = web.Application()
 
-        server = AsyncHttpServer(addr,
-                                 port,
-                                 log_file=log_file,
-                                 verbose=verbose)
+    app.add_routes(routes)
 
-        if (True == disable_cache):
-            server.disable_caching()
-
-        api_list = [
-            server.get("/search", handler.search, DEF_CACHE_TIMEOUT),
-            server.get("/opensearch.xml", handler.opensearch),
-            server.get("/api/search", handler.api_search, DEF_CACHE_TIMEOUT),
-            server.get("/api/chat", handler.api_chat, DEF_CACHE_TIMEOUT),
-        ]
-
-        server.add_routes(api_list)
-        server.set_static_route(handler.static_handler)
-
-        await server.server_forever()
+    web.run_app(app, port=port, host=addr)  # type: ignore
 
 
 def main() -> int:
@@ -418,28 +293,14 @@ def main() -> int:
                         default=DEF_ADDR,
                         help=f"addr to listen on. Default is {DEF_ADDR}")
 
-    parser.add_argument("-v",
-                        "--verbose",
-                        action="store_true",
-                        help="Verbose. Log to stdout.")
-
-    parser.add_argument("--disable-cache",
-                        action="store_true",
-                        help="Disable caching.")
-
     args = parser.parse_args()
 
     print("gsearch:")
     printkv("Listening Address", args.addr)
     printkv("Listening Port", args.port)
-    printkv("Verbose", args.verbose)
-    printkv("Cache Disabled", args.disable_cache)
 
     try:
-        asyncio.run(run_server(args.addr,
-                               args.port,
-                               args.verbose,
-                               args.disable_cache))
+        run_server(args.addr, args.port)
         status = 0
     except FileNotFoundError as e:
         print(e)
